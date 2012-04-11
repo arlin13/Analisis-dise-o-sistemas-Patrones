d4431cd5fbf190fdd7b8f13470ea3c1f2f570a64
Arlin Grijalba <arlin.grijalba@hotmail.com>
2012-04-11 14:46:31 -0700

    Nuevo patrón de diseño aplicado: Strategy

---
+++ Pasteleria/Pasteleria/FormaDePago/ConcreteStrategy/Efectivo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pasteleria//.FormaDePago.ConcreteStrategy
+{
+    class Efectivo : FormaDePago
+    {
+        public override int TotalAPagar(int total)
+        {
+            return 100;
+        }   
+    }
+}
